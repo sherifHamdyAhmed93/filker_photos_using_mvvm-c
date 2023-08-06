45939a8d81a15980537f33ca746fb5b8264a0ebd
Sherif Hamdy <shamdy@xapps.co>
2023-08-06 22:36:40 +0300

    add home view controller and configure collection view

---
+++ Filker Photos using mvvm-c/Filker Photos using mvvm-c/Scene/Home/View/HomeVC + CollectionView.swift
@@ -0,0 +1,109 @@
+//
+//  HomeVC + CollectionView.swift
+//  Filker Photos using mvvm-c
+//
+//  Created by Sherif Hamdy on 06/08/2023.
+//
+
+import UIKit
+
+//MARK:- Configure Datasource
+extension HomeVC{
+    internal func makeDataSource() -> DataSource {
+        // 1
+        let dataSource = DataSource(
+            collectionView: collectionView,
+            cellProvider: { [weak self](collectionView, indexPath, video) ->
+                UICollectionViewCell? in
+                guard let self = self else{return nil}
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.reuseIdentifier, for: indexPath) as! ImageCollectionViewCell
+                return cell
+            })
+        
+        
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader else {
+                return nil
+            }
+            let section = self.dataSource.snapshot()
+                .sectionIdentifiers[indexPath.section]
+            let view = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: BannerCollectionReusableView.reuseIdentifier,
+                for: indexPath) as? BannerCollectionReusableView
+            return view
+        }
+        
+        
+        return dataSource
+    }
+    
+    
+}
+
+extension HomeVC{
+    internal func setupCollectionView(){
+        collectionView.delegate = self
+        //        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        self.collectionView.register(ImageCollectionViewCell.nib, forCellWithReuseIdentifier: ImageCollectionViewCell.reuseIdentifier)
+        
+        collectionView.register(
+            BannerCollectionReusableView.nib,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: BannerCollectionReusableView.reuseIdentifier
+        )
+        
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "footer")
+        
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical //.horizontal
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        collectionView.setCollectionViewLayout(layout, animated: true)
+        addRefreshAndTarget()
+    }
+    
+    private func addRefreshAndTarget(){
+        self.collectionView.addRefresherToCollectionView()
+        self.collectionView.refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+    }
+    
+    @objc private func refresh(){
+    }
+}
+
+extension HomeVC:UICollectionViewDelegate{
+    
+   
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+extension HomeVC:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        //here your custom value for spacing
+        return UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let lay = collectionViewLayout as! UICollectionViewFlowLayout
+        let widthPerItem = (collectionView.frame.width/2) - (lay.minimumInteritemSpacing/2) - lay.sectionInset.left
+        return CGSize(width:widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let availableWidth = view.frame.width
+        let heightPerItem = availableWidth * 1/3
+        return CGSize(width: availableWidth, height: heightPerItem)
+    }
+}
---
+++ Filker Photos using mvvm-c/Filker Photos using mvvm-c/Scene/Home/View/HomeVC.swift
@@ -0,0 +1,47 @@
+//
+//  HomeVC.swift
+//  Filker Photos using mvvm-c
+//
+//  Created by Sherif Hamdy on 06/08/2023.
+//
+
+import UIKit
+import Combine
+
+class HomeVC: UIViewController {
+    
+    //MARK:- IBOutlet
+    @IBOutlet weak var collectionView:UICollectionView!
+    @IBOutlet weak var activityIndicator:UIActivityIndicatorView!
+    
+    //MARK:- CollectionView Attributes
+    let spacing:CGFloat = 15
+    let itemsPerRow:CGFloat = 2
+    lazy var availableWidth = collectionView.frame.width - (spacing * 2) - (spacing*(itemsPerRow-1)/2)
+    lazy var itemWidth = availableWidth / 2
+    
+    //MARK:- typealias
+    typealias DataSource = UICollectionViewDiffableDataSource<Banner, PhotoViewModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Banner, PhotoViewModel>
+    
+    internal lazy var dataSource = makeDataSource()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+        configureActivityIndictor()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configureActivityIndictor(){
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.tintColor = .red
+        activityIndicator.style = .large
+    }
+
+}
